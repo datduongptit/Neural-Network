82a9eed64d8cdf46dc9977dacd1b12ac4320313f
Jason Zisheng Chang <j.zisheng@gmail.com>
2019-04-01 12:38:39 -0400

    Update xornn.py

--- xornn.py
+++ xornn.py
@@ -1,29 +1,62 @@
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 class Neural_Network(object):
+    '''
+    Neural Network Class:
+    Implementation of a Neural Network with two hidden layers with 5 nodes each
+    '''
     # Set this value to true for print statements
     debug = True
-    def __init__(self):
+    def __init__(self,MAXITER=1000):
         # Sets up neural network layers
         np.random.seed(9)
-        self.inputLayerSize = 2     # No. of inputs the neural network takes in
-        self.hiddenLayerSize = 2    # No. of node in hidden layers
-        self.outputLayerSize = 1    # No. of outputs of NN
-        self.learningRate = 3     # Learning rate
+        self.inputLayerSize = 2      # No. of inputs the neural network takes in
+        self.hiddenLayerSize1 = 100    # No. of node in hidden layers
+        self.hiddenLayerSize2 = 100
+        self.outputLayerSize = 1     # No. of outputs of NN
+        self.learningRate = 0.01      # Learning rate
+        
+        self.MAXITER = MAXITER
 
         # Initialize weights to random values
-        self.W1 = np.random.randn(self.inputLayerSize+1,self.hiddenLayerSize)
-        self.W2 = np.random.randn(self.hiddenLayerSize,self.outputLayerSize)
+        self.W1 = np.random.randn(self.inputLayerSize+1,self.hiddenLayerSize1)
+        self.W2 = np.random.randn(self.hiddenLayerSize1,self.hiddenLayerSize2)
+        self.W3 = np.random.randn(self.hiddenLayerSize2,self.outputLayerSize)
 
     # Feedforward Function
     def forward(self, X):
         # Propogate inputs through network
         self.z2 = np.dot(X,self.W1)
         self.a2 = self.sigmoid(self.z2)
+        
         self.z3 = np.dot(self.a2,self.W2)
-        self.yHat = self.sigmoid(self.z3)
+        self.a3 = self.sigmoid(self.z3)
+        
+        self.z4 = np.dot(self.a3,self.W3)
+        self.yHat = self.sigmoid(self.z4)
         return self.yHat
+    
+    def predict(self,X):
+        # Propogate inputs through network
+        self.z2 = np.dot(X,self.W1)
+        self.a2 = self.sigmoid(self.z2)
+        
+        self.z3 = np.dot(self.a2,self.W2)
+        self.a3 = self.sigmoid(self.z3)
+        
+        self.z4 = np.dot(self.a3,self.W3)
+        self.yHat = self.sigmoid(self.z4)
+        yThresh = self.yHat
+        yThresh[yThresh >= 0.5] = 1
+        yThresh[yThresh < 0.5] = 0
+        return self.yHat
+
+    def predict_proba(self, X):
+        # Propogate inputs through network
+        return self.predict(self,X)
 
     # Activation Function
     def sigmoid(self,z):
@@ -40,10 +73,15 @@ class Neural_Network(object):
         self.yHat = self.forward(X)
         # Backpropagates error to weights from the hidden layer
         # to the output
-        delta3 = np.multiply((y-self.yHat), self.sigmoidPrime(self.z3))
+        delta4 = np.multiply((y-self.yHat), self.sigmoidPrime(self.z4))
+        djdW3 = np.dot(self.a3.T,delta4)
+        self.W3 += djdW3*self.learningRate
+        
+        delta3 = np.multiply(np.dot(delta4,self.W3.T),self.sigmoidPrime(self.z3))
         djdW2 = np.dot(self.a2.T,delta3)
+        #print(djdW2.shape)
         self.W2 += djdW2*self.learningRate
-        # Backpropagates error to weights in the first layer to the hidden layer
+
         delta2 = np.multiply(np.dot(delta3,self.W2.T),self.sigmoidPrime(self.z2))
         djdW1 = np.dot(X.T,delta2)
         self.W1 += djdW1*self.learningRate
@@ -59,56 +97,95 @@ class Neural_Network(object):
         delta = np.multiply((y-self.yHat),self.sigmoidPrime(self.z1))
         djdW1 = np.dot(np.transpose(X),delta)
         return djdW1
-
-# For graphing the neural network
-def graph(formula, x_range,m):  
-    x = np.array(x_range)  
-    y = formula(x,m)  # <- note now we're calling the function 'formula' with x
-    plt.plot(x, y,alpha=0.5, linewidth=5)
-
-# Hidden Layer transformation into linear function
-# where x is the input, and m is the hidden layer matrix of shape 3x1
-def hiddenLayer(x,m):
-    print(m)
-    return -1*(m[0]/m[1])*x - (m[2]/m[1])
     
-def main():
-    # Create neural network object class
-    NN = Neural_Network()
-    X = np.matrix("0 0 1; 0 1 1; 1 0 1; 1 1 1")
-    y = np.matrix("1; 0; 0; 1")
-    print("-- error before---- ")
-    print(NN.errorFunction(X,y))
-    for i in range(120):
-        plt.clf()
-        plt.cla()
+    def fit(self,X,y):
+        costHist = []
+        for i in range(self.MAXITER):
+            self.deltaTrain(X,y)
+            costHist.append(self.errorFunction(X,y))
+            pass
+        pass
 
-        NN.deltaTrain(X,y)
-        graph(hiddenLayer,range(0,2),NN.W1[:,0])
-        graph(hiddenLayer,range(0,2),NN.W1[:,1])
-        print(i)
 
-        plt.xlabel("x1")
-        plt.ylabel("x2")
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_moons
+# Create neural network object class
+X,y = make_moons(n_samples=600,noise=0.2)
+bias = np.ones((X.shape[0],1))
+X = np.hstack((X,bias))
+y_ = y
+y = y.reshape(-1,1)
 
-        
-        plt.scatter(0,0,c='red')
-        plt.scatter(0,1,c='black')
-        plt.scatter(1,0,c='black')
-        plt.scatter(1,1,c='red')
-        plt.pause(0.0000000001)
-        
+XTrain = X[:500]
+yTrain = y[:500]
+XTest = X[500:]
+yTest = y[500:]
+
+plt.figure(figsize=(10,8))
+plt.title("Training Data")
+colors = ['r','b']
+for i in range(2):
+    idx = np.where(yTrain==i)
+    plt.scatter(XTrain[idx,0],XTrain[idx,1],c=colors[i],label=i)
+plt.xlabel("x1")
+plt.ylabel("y1")
+plt.legend()
+plt.show()
+
+plt.figure(figsize=(10,8))
+plt.title("Testing Data")
+colors = ['r','b']
+for i in range(2):
+    idx = np.where(yTest==i)
+    plt.scatter(XTest[idx,0],XTest[idx,1],c=colors[i],label=i)
+plt.xlabel("x1")
+plt.ylabel("y1")
+plt.legend()
+plt.show()
+
+NN = Neural_Network()
+print("-- error before on testing---- ")
+print(NN.errorFunction(XTest,yTest))
+NN.fit(X,y)
+print("-- error after on testing---- ")
+print(NN.errorFunction(XTest,yTest))
+
+from matplotlib.colors import ListedColormap
+
+def graphDecisionBounds(X,Y,NN_):
+    cmap_light = ListedColormap(['#FFAAAA', '#AAAAFF'])
+    cmap_bold = ListedColormap(['#FF0000', '#0000FF'])
+    h = 0.02
+    xx,yy = np.meshgrid(np.arange(-50,50),np.arange(-50,50))
+
+    X_ = X
+    Y_ = Y
+
+    x_min, x_max = X_[:, 0].min() - 1, X_[:, 0].max() + 1
+    y_min, y_max = X_[:, 1].min() - 1, X_[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+
+    xx_ = np.c_[xx.ravel(), yy.ravel(),np.ones((xx.ravel().shape[0],1))]
+    Z = NN_.forward(xx_)
+
+    Z = Z.reshape(xx.shape)
+    plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+
+    #plt.scatter(X_[:, 0], X_[:, 1], c=y[:100], cmap=cmap_bold
+    
+    plt.scatter(X_[:, 0], X_[:, 1], c=Y_.flatten(), cmap=cmap_bold)
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+    plt.xlabel("x1")
+    plt.ylabel("x2")
     plt.show()
-    print("-- error after")
-    print(NN.errorFunction(X,y))
-    print("-- new weights")
-    print("-- NN W1")
-    print(NN.W1)
-    print("-- NN W2")
-    print(NN.W2)
 
+plt.figure(figsize=(10,8))
+plt.title("Decision Boundary on Training Set (Red = 1, Blue = 0)")
+graphDecisionBounds(XTrain,yTrain,NN)
 
-if __name__ == "__main__":
-    main()
+plt.figure(figsize=(10,8))
+plt.title("Decision Boundary on Testing Set (Red = 1, Blue = 0)")
+graphDecisionBounds(XTest,yTest,NN)
 
-    
